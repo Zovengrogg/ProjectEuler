5097623c21e26f61b4105a3bd7d2ce036adfc2c9
Zovengrogg <mitchelcharette@gmail.com>
2024-02-15 20:58:17 -0500

    euler3 sieve and first run through

--- Prolog/euler3.pl
+++ Prolog/euler3.pl
@@ -4,3 +4,33 @@
     What is the largest prime factor of the number 600851475143?
 */
 
+
+sieve(N, Primes) :-
+    Y is floor(sqrt(N)),
+    numlist(2, Y, List),
+    sieve(List, Primes).
+
+sieve([], []).
+sieve([P|T], [P|Primes]) :-
+    filtered(P, T, Filtered),
+    sieve(Filtered, Primes).
+
+filtered(_, [], []).
+filtered(P, [X|T], [X|Filtered]) :-
+    X mod P =\= 0,
+    filter(P, T, Filtered).
+filter(P, [X|T], Filtered) :-
+    X mod P =:= 0,
+    filter(P, T, Filtered).
+
+
+largestPrimeFactor(N, Result) :-
+    sieve(N, Primes),
+    reverse(Primes, RPrimes),
+    largestPrimeFactor(RPrimes, N, Result).
+
+largestPrimeFactor([P|_], N, P) :-
+    N mod P =:= 0.
+largestPrimeFactor([P|T], N, Result) :-
+    N mod P =\= 0,
+    largestPrimeFactor(T, N, Result).
